4cea80473811d10d12e35d8cc9821674b83f9839
yuunik <chorria.zhou@gmail.com>
2025-02-17 16:59:40 +0800

    feat: add uni-ui config.

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 //
+const change = () => {}
 </script>
 
 <template>
-  <view class="index">index</view>
+  <uni-calendar
+    :insert="true"
+    :lunar="true"
+    :start-date="'2019-3-2'"
+    :end-date="'2019-5-20'"
+    @change="change"
+  />
 </template>
 
 <style lang="scss">
